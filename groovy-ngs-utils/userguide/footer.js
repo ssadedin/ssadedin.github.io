5eff041a02972d42e1ebc961aa0f64b8e519deb9
Simon Sadedin <ssadedin@gmail.com>
2023-04-16 08:39:48 +1000

    add znai docs deployment

---
+++ groovy-ngs-utils/userguide/footer.js
@@ -0,0 +1,10 @@
+footer = {
+  "type" : "Footer",
+  "content" : [ {
+    "type" : "Paragraph",
+    "content" : [ {
+      "text" : "Thank you for using Groovy NGS!",
+      "type" : "SimpleText"
+    } ]
+  } ]
+}
---
+++ groovy-ngs-utils/userguide/toc.js
@@ -0,0 +1,231 @@
+toc = [ {
+  "chapterTitle" : "",
+  "dirName" : "",
+  "items" : [ {
+    "chapterTitle" : "",
+    "pageTitle" : "",
+    "pageMeta" : { },
+    "dirName" : "",
+    "fileName" : "index",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Groovy NGS",
+      "id" : "groovy-ngs"
+    } ]
+  } ]
+}, {
+  "chapterTitle" : "Chapter One",
+  "dirName" : "chapter-one",
+  "items" : [ {
+    "chapterTitle" : "Chapter One",
+    "pageTitle" : "Introduction",
+    "pageMeta" : { },
+    "dirName" : "chapter-one",
+    "fileName" : "introduction",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "What is Groovy NGS?",
+      "id" : "what-is-groovy-ngs"
+    }, {
+      "title" : "Why Groovy?",
+      "id" : "why-groovy"
+    }, {
+      "title" : "Structure",
+      "id" : "structure"
+    }, {
+      "title" : "API Documentation",
+      "id" : "api-documentation"
+    } ]
+  }, {
+    "chapterTitle" : "Chapter One",
+    "pageTitle" : "How To Use Groovy NGS",
+    "pageMeta" : { },
+    "dirName" : "chapter-one",
+    "fileName" : "how-to-use-groovy-NGS",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "As a Dependency In Projects",
+      "id" : "as-a-dependency-in-projects"
+    }, {
+      "title" : "On the Command Line",
+      "id" : "on-the-command-line"
+    }, {
+      "title" : "Interactively via a Groovy Shell",
+      "id" : "interactively-via-a-groovy-shell"
+    }, {
+      "title" : "In a Jupyter Notebook",
+      "id" : "in-a-jupyter-notebook"
+    } ]
+  } ]
+}, {
+  "chapterTitle" : "Chapter 2 Genomic Regions",
+  "dirName" : "chapter-2-genomic-regions",
+  "items" : [ {
+    "chapterTitle" : "Chapter 2 Genomic Regions",
+    "pageTitle" : "Regions And Ranges",
+    "pageMeta" : { },
+    "dirName" : "chapter-2-genomic-regions",
+    "fileName" : "regions-and-ranges",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Regions and Ranges",
+      "id" : "regions-and-ranges"
+    } ]
+  }, {
+    "chapterTitle" : "Chapter 2 Genomic Regions",
+    "pageTitle" : "Common Operations",
+    "pageMeta" : { },
+    "dirName" : "chapter-2-genomic-regions",
+    "fileName" : "common-operations",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Creating Regions Objects",
+      "id" : "creating-regions-objects"
+    }, {
+      "title" : "Basic Metrics",
+      "id" : "basic-metrics"
+    }, {
+      "title" : "Treating as a Collection",
+      "id" : "treating-as-a-collection"
+    }, {
+      "title" : "Finding Overlaps",
+      "id" : "finding-overlaps"
+    }, {
+      "title" : "Intersection",
+      "id" : "intersection"
+    }, {
+      "title" : "Flattening",
+      "id" : "flattening"
+    }, {
+      "title" : "Assigning Properties",
+      "id" : "assigning-properties"
+    } ]
+  }, {
+    "chapterTitle" : "Chapter 2 Genomic Regions",
+    "pageTitle" : "Loading And Saving",
+    "pageMeta" : { },
+    "dirName" : "chapter-2-genomic-regions",
+    "fileName" : "loading-and-saving",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Loading BED files",
+      "id" : "loading-bed-files"
+    }, {
+      "title" : "Saving BED files",
+      "id" : "saving-bed-files"
+    }, {
+      "title" : "Region Based Data Tables",
+      "id" : "region-based-data-tables"
+    } ]
+  } ]
+}, {
+  "chapterTitle" : "Chapter 3 Vcf Files",
+  "dirName" : "chapter-3-vcf-files",
+  "items" : [ {
+    "chapterTitle" : "Chapter 3 Vcf Files",
+    "pageTitle" : "Parsing Vcf Files",
+    "pageMeta" : { },
+    "dirName" : "chapter-3-vcf-files",
+    "fileName" : "parsing-vcf-files",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Loading a VCF File",
+      "id" : "loading-a-vcf-file"
+    }, {
+      "title" : "Accessing Header Information Only",
+      "id" : "accessing-header-information-only"
+    }, {
+      "title" : "Streaming Processing",
+      "id" : "streaming-processing"
+    }, {
+      "title" : "Processing Variants in VCFs",
+      "id" : "processing-variants-in-vcfs"
+    }, {
+      "title" : "Indexed VCFs",
+      "id" : "indexed-vcfs"
+    } ]
+  }, {
+    "chapterTitle" : "Chapter 3 Vcf Files",
+    "pageTitle" : "Working With Variants",
+    "pageMeta" : { },
+    "dirName" : "chapter-3-vcf-files",
+    "fileName" : "working-with-variants",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "General Notes",
+      "id" : "general-notes"
+    }, {
+      "title" : "Variant Properties",
+      "id" : "variant-properties"
+    }, {
+      "title" : "Variants as Regions",
+      "id" : "variants-as-regions"
+    }, {
+      "title" : "Querying VCFs for Presence of Variants",
+      "id" : "querying-vcfs-for-presence-of-variants"
+    }, {
+      "title" : "Info Fields",
+      "id" : "info-fields"
+    }, {
+      "title" : "Annotated Variants",
+      "id" : "annotated-variants"
+    }, {
+      "title" : "Updating Variant Attributes",
+      "id" : "updating-variant-attributes"
+    } ]
+  } ]
+}, {
+  "chapterTitle" : "Chapter 4 Alignment Files",
+  "dirName" : "chapter-4-alignment-files",
+  "items" : [ {
+    "chapterTitle" : "Chapter 4 Alignment Files",
+    "pageTitle" : "Opening And Reading Alignment Files",
+    "pageMeta" : { },
+    "dirName" : "chapter-4-alignment-files",
+    "fileName" : "opening-and-reading-alignment-files",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Introduction",
+      "id" : "introduction"
+    }, {
+      "title" : "Opening BAM and CRAM Files",
+      "id" : "opening-bam-and-cram-files"
+    }, {
+      "title" : "Accessing Reads",
+      "id" : "accessing-reads"
+    }, {
+      "title" : "Accessing Paired Reads",
+      "id" : "accessing-paired-reads"
+    }, {
+      "title" : "Generating Pileups",
+      "id" : "generating-pileups"
+    }, {
+      "title" : "Calculating Coverage Depth",
+      "id" : "calculating-coverage-depth"
+    } ]
+  } ]
+}, {
+  "chapterTitle" : "Chapter 5 Miscellaneous Utilities",
+  "dirName" : "chapter-5-miscellaneous-utilities",
+  "items" : [ {
+    "chapterTitle" : "Chapter 5 Miscellaneous Utilities",
+    "pageTitle" : "Miscellaneous Utilities",
+    "pageMeta" : { },
+    "dirName" : "chapter-5-miscellaneous-utilities",
+    "fileName" : "miscellaneous-utilities",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Creating Readers, Writers and Streams",
+      "id" : "creating-readers-writers-and-streams"
+    }, {
+      "title" : "Formatting Tables",
+      "id" : "formatting-tables"
+    }, {
+      "title" : "JupyterLab Support",
+      "id" : "jupyterlab-support"
+    }, {
+      "title" : "RefGene Database Access",
+      "id" : "refgene-database-access"
+    } ]
+  } ]
+} ]
